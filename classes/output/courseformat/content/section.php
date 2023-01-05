7e776154f24e16d244d4cf5146da2c887837793a
John Maydew <jdmayd@essex.ac.uk>
2023-01-05 15:26:31 +0000

    Fixed drop-shadow for highlighted cards not following the border-radius Used wrong property when trying to determine if a user could see that a section isn't available to them, so added the uservisible property

--- classes/output/courseformat/content/section.php
+++ classes/output/courseformat/content/section.php
@@ -54,10 +54,8 @@ class section extends section_base {
         $data = parent::export_for_template($output);
         $data->classes = [];
 
-        if ((object_property_exists($data, "hiddenfromstudents")
-            && $data->hiddenfromstudents)
-            || $data->restrictionlock
-        ) {
+        if (object_property_exists($data, "hiddenfromstudents")
+            && $data->hiddenfromstudents) {
             $data->classes[] = "hiddenfromstudents";
         }
 
@@ -75,6 +73,11 @@ class section extends section_base {
         $data->highlighted = $course->marker == $this->section->section;
         if ($data->highlighted) {
             $data->classes[] = "highlighted";
+        }
+
+        $data->uservisible = $this->section->uservisible;
+        if (!$data->uservisible) {
+            $data->classes[] = "hiddenfromme";
         }
 
         // Don't show the "insert new topic" button after every section in editing mode.
