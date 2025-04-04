358c23b6f06e0f2f9b22711fd776312e969b2b00
John Maydew <jdmayd@essex.ac.uk>
2025-04-04 14:11:55 +0100

    Added subsection navigation to the optional top and bottom section navigation bars

--- classes/output/courseformat/content/sectionnavigation.php
+++ classes/output/courseformat/content/sectionnavigation.php
@@ -25,8 +25,13 @@
 
 namespace format_cards\output\courseformat\content;
 
+use cm_info;
+use context_course;
+use core\exception\coding_exception;
 use core_courseformat\output\local\content\sectionnavigation as sectionnavigation_base;
+use moodle_exception;
 use renderer_base;
+use section_info;
 use stdClass;
 
 /**
@@ -54,8 +59,10 @@ class sectionnavigation extends sectionnavigation_base {
      *
      * @param renderer_base $output typically, the renderer that's calling this function
      * @return stdClass data context for a mustache template
+     * @throws moodle_exception
      */
     public function export_for_template(renderer_base $output): stdClass {
+        global $CFG, $DB;
 
         $data = parent::export_for_template($output);
 
@@ -75,6 +82,142 @@ class sectionnavigation extends sectionnavigation_base {
             $data->hasprevious = false;
         }
 
+        // Moodle 4.5 introduces subsections, which add another layer of navigation.
+        // If we're currently _in_ a subsection, then we need to be able to navigate 'up' out of it.
+        if ($CFG->version >= 2024100700) {
+            $this->add_subsection_data($data);
+        }
+
         return $data;
+    }
+
+    /**
+     * Given the cm_info for a subsection, get the course_section it refers to.
+     *
+     * @param cm_info $cminfo
+     * @return section_info
+     */
+    protected function get_sectioninfo_from_subsection(cm_info $cminfo): section_info {
+        if ($cminfo->modname !== 'subsection') {
+            throw new coding_exception("Course module with id $cminfo->id is not a subsection");
+        }
+
+        $modinfo = $this->format->get_modinfo();
+
+        foreach ($modinfo->get_section_info_all() as $section) {
+            if ($section->component !== 'mod_subsection') {
+                continue;
+            }
+
+            if ((int) $section->itemid === (int) $cminfo->instance) {
+                return $section;
+            }
+        }
+
+        throw new coding_exception("Course module with id $cminfo->id couldn't be matched to a course_section");
+    }
+
+    /**
+     * If the current section is a subsection, adds additional navigation data
+     *
+     * @param stdClass $data
+     * @return void
+     * @throws moodle_exception
+     */
+    protected function add_subsection_data(stdClass &$data): void {
+        $data->hasparent = false;
+        $modinfo = $this->format->get_modinfo();
+
+        $currentsection = $modinfo->get_section_info($this->sectionno, MUST_EXIST);
+
+        // Nothing to do if the current section isn't a delegated mod_subsection.
+        if (!$currentsection->is_delegated() || $currentsection->component !== 'mod_subsection') {
+            return;
+        }
+
+        $parentsection = false;
+        $ourposition = -1;
+
+        foreach ($modinfo->get_section_info_all() as $sectioninfo) {
+
+            // Ignore section #0.
+            if ((int) $sectioninfo->sectionnum === 0) {
+                continue;
+            }
+
+            // Does this section contain a mod_subsection where the instanceid matches the itemid of the
+            // subsection we're currently in?
+            foreach ($sectioninfo->get_sequence_cm_infos() as $index => $sibling) {
+                if ((int) $sibling->instance !== (int) $currentsection->itemid) {
+                    continue;
+                }
+
+                $ourposition = $index;
+            }
+
+            if ($ourposition === -1) {
+                continue;
+            }
+
+            $data->hasparent = $sectioninfo->uservisible;
+            $data->parenturl = $this->format->get_view_url($sectioninfo);
+            $data->parentname = $sectioninfo->name;
+            $data->parenthidden = !$sectioninfo->visible;
+
+            $parentsection = $sectioninfo;
+            break;
+        }
+
+        if ($parentsection === false || $ourposition === -1) {
+            throw new coding_exception("Section with ID $currentsection->id is an orphaned subsection");
+        }
+
+        // If we're in a subsection, the previous and next links only make sense if the current section's
+        // immediate siblings are also subsections.
+
+        $data->hasprevious = false;
+        $data->hasnext = false;
+
+        // Search back in the sequence to find a subsection we can view.
+        for ($i = $ourposition - 1; $i >= 0; $i--) {
+            $cminfo = $parentsection->get_sequence_cm_infos()[$i];
+
+            // Not a subsection, stop searching.
+            if ($cminfo->modname !== 'subsection') {
+                break;
+            }
+
+            $section = $this->get_sectioninfo_from_subsection($cminfo);
+
+            if ($section->uservisible) {
+                $data->hasprevious = true;
+                $data->previousurl = $this->format->get_view_url($section);
+                $data->previousname = $section->name;
+                $data->previoushidden = !$section->visible;
+
+                break;
+            }
+        }
+
+        // Search forward in the sequence to find the next subsection we can view.
+        for ($i = $ourposition + 1; $i < count($parentsection->get_sequence_cm_infos()); $i++) {
+            $cminfo = $parentsection->get_sequence_cm_infos()[$i];
+
+            // Not a subsection, stop searching.
+            if ($cminfo->modname !== 'subsection') {
+                break;
+            }
+
+            $section = $this->get_sectioninfo_from_subsection($cminfo);
+
+            if ($section->uservisible) {
+                $data->hasnext = true;
+                $data->nexturl = $this->format->get_view_url($section);
+                $data->nextname = $section->name;
+                $data->nexthidden = !$section->visible;
+
+                break;
+            }
+        }
     }
 }
--- tests/behat/subsection.feature
+++ tests/behat/subsection.feature
@@ -1,8 +1,8 @@
 @format @format_cards
-Feature: Users view subsections on course page
-  In order to use subsections
-  As an user
-  I need to view subsections on course page
+Feature: Subsection support
+  In order to use courses using the cards format
+  As a user
+  I can view and navigate through subsections
 
   Background:
     Given I enable "subsection" "mod" plugin
@@ -18,17 +18,72 @@ Feature: Users view subsections on course page
       | teacher1 | C1     | editingteacher |
       | student1 | C1     | student        |
     And the following "activities" exist:
-      | activity   | name                 | course | idnumber | section |
-      | subsection | Subsection1          | C1     | sub1     | 1       |
-      | page       | Page1 in Subsection1 | C1     | page11   | 4       |
-      | subsection | Subsection2          | C1     | sub2     | 1       |
-      | data       | New database         | C1     | data1    | 3       |
-      | page       | New page             | C1     | page1    | 3       |
+      | activity   | name                 | course | idnumber | section | visible |
+      | subsection | Subsection1          | C1     | sub1     | 1       | 1       |
+      | page       | Page1 in Subsection1 | C1     | page11   | 4       | 1       |
+      | subsection | Subsection2          | C1     | sub2     | 1       | 0       |
+      | page       | Page in Subsection2  | C1     | page22   | 5       | 1       |
+      | subsection | Subsection3          | C1     | sub3     | 1       | 1       |
+      | page       | Page in Subsection3  | C1     | page33   | 6       | 1       |
+      | data       | New database         | C1     | data1    | 3       | 1       |
+      | page       | New page             | C1     | page1    | 3       | 1       |
 
   @javascript @moodle_405_and_after
   Scenario: A user can view and navigate to a subsection displayed on a course page
     Given I log in as "student1"
     When I am on "Course 1" course homepage
+    Then I should not see "Subsection1" in the "region-main" "region"
+    When I am on the "Course 1 > Section 1" "format_cards > section" page
     Then I should see "Subsection1" in the "region-main" "region"
     When I click on "Subsection1" "link" in the "Subsection1" "activity"
     Then I should see "Page1 in Subsection1" in the "region-main" "region"
+
+  @javscript @moodle_405_and_after
+  Scenario: The section navigation region for a subsection contains a button to go to the parent section
+    Given the following config values are set as admin:
+      | sectionnavigation | 4 | format_cards |
+    And I log in as "student1"
+    And I am on the "Course 1 > Section 1" "format_cards > section" page
+    Then "[data-action=parentsection]" "css_element" should not exist in the "sectionnavigation-top" "region"
+    And "[data-action=parentsection]" "css_element" should not exist in the "sectionnavigation-bottom" "region"
+    When I click on "Subsection1" "link" in the "Subsection1" "activity"
+    Then "Go to Section 1" "icon" should exist in the "sectionnavigation-top" "region"
+    And "Go to Section 1" "icon" should exist in the "sectionnavigation-bottom" "region"
+    When I click on "Go to Section 1" "icon" in the "sectionnavigation-top" "region"
+    And I click on "Subsection1" "link" in the "Subsection1" "activity"
+    And I click on "Go to Section 1" "icon" in the "sectionnavigation-bottom" "region"
+    Then "Subsection1" "activity" should exist
+
+  @javscript @moodle_405_and_after
+  Scenario Outline: I can navigate between subsections
+    Given the following config values are set as admin:
+      | sectionnavigation | 4 | format_cards |
+    And I log in as "<user>"
+    When I am on the "Course 1 > Section 1" "format_cards > section" page
+    Then "Subsection2" "activity" <should or not> be visible
+
+    When I click on "Subsection1" "link" in the "Subsection1" "activity"
+    Then ".prevsection [data-action=previoussection]" "css_element" should not exist
+    And I click on "Go to <subsection after subsection1>" "icon" in the "sectionnavigation-top" "region"
+    Then "Page in <subsection after subsection1>" "activity" should exist in the "region-main" "region"
+
+    When I am on the "Course 1 > Section 1" "format_cards > section" page
+    And I click on "Subsection1" "link" in the "Subsection1" "activity"
+    And I click on "Go to <subsection after subsection1>" "icon" in the "sectionnavigation-bottom" "region"
+    Then "Page in <subsection after subsection1>" "activity" should exist in the "region-main" "region"
+
+    When I am on the "Course 1 > Section 1" "format_cards > section" page
+    And I click on "Subsection3" "link" in the "Subsection3" "activity"
+    Then ".nextsection [data-action=nextsection]" "css_element" should not exist
+    And I click on "Go to <subsection before subsection3>" "icon" in the "sectionnavigation-top" "region"
+    Then "Page in <subsection before subsection3>" "activity" should exist in the "region-main" "region"
+
+    When I am on the "Course 1 > Section 1" "format_cards > section" page
+    And I click on "Subsection3" "link" in the "Subsection3" "activity"
+    And I click on "Go to <subsection before subsection3>" "icon" in the "sectionnavigation-bottom" "region"
+    Then "Page in <subsection before subsection3>" "activity" should exist in the "region-main" "region"
+
+    Examples:
+      | user     | should or not | subsection after subsection1 | subsection before subsection3 |
+      | teacher1 | should        | Subsection2                  | Subsection2                   |
+      | student1 | should not    | Subsection3                  | Subsection1                   |
